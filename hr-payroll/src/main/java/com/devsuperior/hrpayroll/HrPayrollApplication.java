313db03a25af30ea503853974550996915ed706e
DeividsonGonini <deividsongonini@gmail.com>
2020-12-06 14:52:52 -0300

    Feign Client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -2,6 +2,10 @@ package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+//Habilita o Feign
+@EnableFeignClients
 
 @SpringBootApplication
 public class HrPayrollApplication {
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,37 +1,24 @@
 package com.devsuperior.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	//inserir o nome da propriedade criada no application.properties
-	@Value("${hr-worker.host}")
-	//variavel para guardar o valor do @Value
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(Long workerId, int days) {
-		//Criar um mapa/dicionario de Parametros para serem utilizados no RestTemplate 
-		Map<String, String> uriVariables = new HashMap<>();
-		//Inserir o Parametro do ID necessario para a rota (workerId tem que ser convertido para String)
-		uriVariables.put("id", "" + workerId);
 		
-		
-		//Criar a requisição para uma API externa usando RestTemplate
-		//parametros: 1º URL+caminho/parametro/id / 2º Tipo do Objeto / 3º parâmetros
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		/*
+		 * Para acessar o objeto Worker que esta dentro do Response Entity, utilizar o .getBody();
+		 */
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
