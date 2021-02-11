c4cb4d550766abde4341e4a24033aaf313e55636
DeividsonGonini <deividsongonini@gmail.com>
2021-02-11 01:46:45 -0300

    Login JWT Token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -3,26 +3,42 @@ package com.devsuperior.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private UserFeignClient userFeignClient;
 	
+	//Apos implementado o metodo Utilizado pelo UserDetailsService, pode excluir esse, pois fica duplicado, ficara aqui para exemplo
 	public User findByEmail(String email) {
 		User user = userFeignClient.findByEmail(email).getBody();
-		if(user ==null) {
+		if(user == null) {
 			logger.error("Email not found: " + email);
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("Email found: " + email);
+		return user;
+	}
+
+	//Utilizada pelo UserDetailsService do Spring Security para a busca por email
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
 		return user;
 	}
 }
