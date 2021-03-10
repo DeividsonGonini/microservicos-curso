4131cedb9a60efb247e3f49216fbd5dfdf53f449
DeividsonGonini <deividsongonini@gmail.com>
2021-03-10 03:00:04 -0300

    configuracao hr-worker e hr-user em des com mysql

---
+++ hr-worker/create.sql
@@ -0,0 +1,5 @@
+create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=MyISAM;
+create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=MyISAM;
+create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=MyISAM;
+create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=MyISAM;
+create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=MyISAM;
