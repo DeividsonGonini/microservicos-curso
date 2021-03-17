066ebc1680b1bcc4e5dc70ef5364d6cc1bc16550
DeividsonGonini <deividsongonini@gmail.com>
2021-03-17 00:31:21 -0300

    Container References

--- hr-worker/create.sql
+++ hr-worker/create.sql
@@ -3,3 +3,4 @@ create table tb_worker (id bigint not null auto_increment, daily_income double p
 create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=MyISAM;
 create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=MyISAM;
 create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=MyISAM;
+create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=MyISAM;
